34671192ce9309b10140819a1e7eba4996a147f0
Ashu Pant <99981051+ashupant01@users.noreply.github.com>
2022-03-27 23:44:40 +0530

    Add files via upload
    All day 6 files are added in single branch

---
+++ reversenumber.java
@@ -0,0 +1,17 @@
+package day6;
+
+public class reversenumber {
+
+	public static void main(String[] args)   
+	{  
+	int number = 123456, reverse = 0;  
+	while(number != 0)   
+	{  
+	int remainder = number % 10;  
+	reverse = reverse * 10 + remainder;  
+	number = number/10;  
+	}  
+	System.out.println("Reverse of the number is: " + reverse);  
+	}  
+	}  
+
